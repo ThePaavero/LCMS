08188a3425c40ca9bb5b9d9cd24ee5ec5cbe2ca8
Pekka <nospam@astudios.org>
2014-05-21 21:18:53 +0300

    Toolbox link listeners

--- assets/js/lcms/modules/BlockTools.js
+++ assets/js/lcms/modules/BlockTools.js
@@ -3,21 +3,32 @@ LCMS.Modules.BlockTools = function(_block) {
 
 	var block = _block;
 	var box;
+	var block_id;
+
+	var self = this;
 
 	this.init = function()
 	{
+		block_id = block.attr('data-id');
+		removePossibleOldBox();
 		createBox();
+	};
+
+	var removePossibleOldBox = function()
+	{
+		$('.cms_block_tools[data-block_id=' + block_id + ']').remove();
 	};
 
 	var createBox = function()
 	{
 		box = document.createElement('div');
 		box.className = 'cms_block_tools';
+		$(box).attr('data-block_id', block_id);
 
 		var html = '';
-		html += '<a href="#">Save</a>';
-		html += '<a href="#">Cancel</a>';
-		html += '<a href="#">History</a>';
+		html += '<a href="#save">Save</a>';
+		html += '<a href="#cancel">Cancel</a>';
+		html += '<a href="#history">History</a>';
 		html += '</div>';
 
 		box.innerHTML = html;
@@ -30,6 +41,37 @@ LCMS.Modules.BlockTools = function(_block) {
 			'top' : top,
 			'left': left
 		});
+
+		doListenersForBox();
+	};
+
+	var doListenersForBox = function()
+	{
+		var links = $(box).find('a');
+
+		links.on('click', doOnToolLinkClick);
+	};
+
+	var doOnToolLinkClick = function(e)
+	{
+		e.preventDefault();
+		var action = $(this).attr('href').split('#')[1];
+		self[action]();
+	};
+
+	this.save = function()
+	{
+		console.log('Saving');
+	};
+
+	this.cancel = function()
+	{
+		console.log('Canceling');
+	};
+
+	this.history = function()
+	{
+		console.log('History');
 	};
 
 };
